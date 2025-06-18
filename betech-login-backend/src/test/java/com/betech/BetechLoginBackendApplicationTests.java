2223e51dd8368d1a6102693aa804f765c6ab6054
betechinc <betechincorporated@gmail.com>
2025-06-17 21:27:23 -0400

    Update BetechLoginBackendApplicationTests.java

--- betech-login-backend/src/test/java/com/betech/BetechLoginBackendApplicationTests.java
+++ betech-login-backend/src/test/java/com/betech/BetechLoginBackendApplicationTests.java
@@ -1,14 +1,11 @@
 package com.betech;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BetechLoginBackendApplicationTests {
-    // @Test
-    // void contextLoads() {
-    // }
-    public String void(){
-        System.out.println("Test in progress");
-    }
-}
+// @SpringBootTest
+// class BetechLoginBackendApplicationTests {
+//     // @Test
+//     // void contextLoads() {
+//     // }
+// }
