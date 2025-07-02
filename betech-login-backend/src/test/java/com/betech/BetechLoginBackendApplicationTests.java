f644ae057aab88a86f6a00e66e41be9ee882a3ff
DevUser <betechincorporated@gmail.com>
2025-07-02 21:56:43 +0000

    refactored directory structre and jenkinsfile to only pull the needed dir for frontend and backend jobs

--- betech-login-backend/src/test/java/com/betech/BetechLoginBackendApplicationTests.java
+++ betech-login-backend/src/test/java/com/betech/BetechLoginBackendApplicationTests.java
@@ -7,5 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 class BetechLoginBackendApplicationTests {
     @Test
     void contextLoads() {
+      // This method is empty due to the nature of the test.
+      // It is used to check if the Spring application context loads successfully.
+      // If there are any issues with the application context, an exception will be thrown.
+      // No assertions are needed here.
     }
 }
