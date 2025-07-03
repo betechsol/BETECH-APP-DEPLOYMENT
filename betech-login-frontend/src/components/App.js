b35766b6156fde03c113670a28e2c23bfc513a7c
DevUser <betechincorporated@gmail.com>
2025-07-03 01:59:17 +0000

    updated react-router-dom to react-router

--- betech-login-frontend/src/components/App.js
+++ betech-login-frontend/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate, useNavigate } from 'react-router';
 import Login from './components/Login';
 import Register from './components/Register';
 import Home from './components/Home';
