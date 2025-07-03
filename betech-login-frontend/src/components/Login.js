e41c6fdc682ec1359749ebd62be54bf130577973
DevUser <betechincorporated@gmail.com>
2025-07-03 01:59:24 +0000

    updated react-router-dom to react-router

--- betech-login-frontend/src/components/Login.js
+++ betech-login-frontend/src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 
 export default function Login({ onLogin }) {
   const [username, setUsername] = useState('');
